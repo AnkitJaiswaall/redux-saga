0ebb69ce9e4f64f1e51a14123468f24b1251f746
unknown <deepak@Ankit.ebizneeds.local>
2022-08-17 19:10:38 +0530

    redux-saga update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Header from './components/Header';
+import Cart from './components/Cart'
 import Main from './components/Main';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Routes>
+        <Route exact path='/' element={<Main/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        
+      </Routes>
+     
     </div>
   );
 }
--- src/redux/productAction.js
+++ src/redux/productAction.js
@@ -1,17 +1,16 @@
-import { PRODUCT_LIST,SET_PRODUCT_LIST } from "./constant"
+import { PRODUCT_LIST, SEARCH_PRODUCT } from "./constant"
 
-export const productList =() => {
-
+export const productList = () => {
+  
     return {
         type: PRODUCT_LIST,
-        data:"Apple"
-}
+    }
 }
 
-export const SetProductList =(data) => {
-console.log("set action")
+export const productSearch = (query) => {
+  
     return {
-        type: SET_PRODUCT_LIST,
-        data
-}
+        type: SEARCH_PRODUCT,
+        query
+    }
 }
